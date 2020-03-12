a3fa4c0699dea65ea3b65570ac1d5666f92ed729
Eter <eter@gmail.com>
2020-03-12 13:41:23 +0800

    add terror

---
+++ terror/terror.go
@@ -0,0 +1,45 @@
+// Copyright 2020 EOI, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package terror
+
+import (
+	"github.com/eopenio/log"
+	"go.uber.org/zap"
+)
+
+// MustNil cleans up and fatals if err is not nil.
+func MustNil(err error, closeFuns ...func()) {
+	if err != nil {
+		for _, f := range closeFuns {
+			f()
+		}
+		log.Fatal("unexpected error", zap.Error(err))
+	}
+}
+
+// Call executes a function and checks the returned err.
+func Call(fn func() error) {
+	err := fn()
+	if err != nil {
+		log.Error("function call errored", zap.Error(err))
+	}
+}
+
+// Log logs the error if it is not nil.
+func Log(err error) {
+	if err != nil {
+		log.Error("encountered error", zap.Error(err))
+	}
+}
+
